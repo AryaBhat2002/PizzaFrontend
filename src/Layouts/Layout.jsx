2aa830f8696725129f93656d957132bc0db661b8
Arya Sankar Bhattacharyya <aryasbhattacharyya@gmail.com>
2024-07-12 19:05:23 +0530

    Added product slice

--- src/Layouts/Layout.jsx
+++ src/Layouts/Layout.jsx
@@ -3,11 +3,12 @@
 import { useDispatch, useSelector } from 'react-redux';
 import PizzaLogo from '../assets/Images/pizza1.png'
 import Footer from '../Components/Footer';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { logout } from '../Redux/Slices/AuthSlice';
 function Layout({children}) {
 
     const dispatch = useDispatch();
+    const navigate = useNavigate();
 
     const isLoggedIn = useSelector((state) => state.auth.isLoggedIn);
 
@@ -19,8 +20,7 @@ function Layout({children}) {
         <div>
 
             <nav className="flex items-center justify-around h-16 text-[#6B7280] font-mono border-none shadow-md">
-
-                <div className="flex items-center justify-center">
+                <div className="flex items-center justify-center hover:cursor-pointer" onClick={() => navigate('/')}>
                     <p>Pizza App</p>
                     <img src={PizzaLogo} alt="Pizza logo" />
                 </div>
@@ -46,7 +46,7 @@ function Layout({children}) {
 
                 <div>
                     <ul className='flex gap-4'>
-                        <li className='hover: text-[#FF9110]'>
+                        <li className='hover: text-[#b56810]'>
                             {isLoggedIn ? (
                                 <Link onClick={handleLogout}>Logout</Link>
                             ): (
